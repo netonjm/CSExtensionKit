20ad3186aef3b1a43bf506f6cc19294ebf81d776
José Medrano <jose.medrano@xamarin.com>
2014-12-15 08:34:36 +0100

    Fix in scene position to correct objects position

--- example/BeatEmUpGameExample/mac/GameAppDelegate.cs
+++ example/BeatEmUpGameExample/mac/GameAppDelegate.cs
@@ -16,13 +16,17 @@ namespace BeatEmUpGameExample.Mac
 			application.ContentSearchPaths.Add ("images");
 			application.ContentSearchPaths.Add ("sounds");
 
-			CCSize windowSize = mainWindow.WindowSizeInPixels;
-			mainWindow.SetDesignResolutionSize (600, 320, CCSceneResolutionPolicy.ShowAll);
+			CCSize windowSize = new CCSize (600, 320);
+			mainWindow.SetDesignResolutionSize (windowSize.Width, windowSize.Height, CCSceneResolutionPolicy.FixedHeight);
 
 			CCScene scene = new CCScene (mainWindow);
 			StartLevelLayer gameLayer = new StartLevelLayer ();
 			scene.AddChild (gameLayer);
+
 			mainWindow.RunWithScene (scene);
+			//Hack to see all objects in right position ¿? 
+			scene.Position = -windowSize.Center;
+
 		}
 
 		public override void ApplicationDidEnterBackground (CCApplication application)
